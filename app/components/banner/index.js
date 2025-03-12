13673d815b53251d4140b26a372a8c9fa774835e
dariuszstawik <dariusz.stawik@gmail.com>
2025-03-12 02:28:32 +0100

    Update content and styles

--- app/components/banner/index.js
+++ app/components/banner/index.js
@@ -1,9 +1,18 @@
 import { HandHeart } from "lucide-react";
+import Button from "../Button";
 
 export default function Banner({ children, icon }) {
   return (
     <div className="text-white text-md lg:text-lg my-16 bg-orange p-8 rounded-md flex ">
-      {children}
+      <div className="flex flex-col gap-4">
+        {children}
+        <Button
+          title="Umów wizytę"
+          hasSecondBg
+          href="https://dobremiejscefizjoterapiadzieci.booksy.com/"
+        />
+      </div>
+
       <HandHeart size={148} absoluteStrokeWidth className="shrink-0" />
     </div>
   );
