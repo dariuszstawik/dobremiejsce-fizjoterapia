2012b6b0e271aff463d812beca0afc1cc56da9e7
dariuszstawik <dariusz.stawik@gmail.com>
2026-01-15 01:21:04 +0100

    Update content on zespol page

--- app/zespol/page.js
+++ app/zespol/page.js
@@ -285,15 +285,6 @@ export default function Zespol() {
                 </span>{" "}
               </h1>
 
-              {/* <div className="mb-6 text:lg md:text-xl lg:text-2xl max-w-5xl ">
-                <div className="flex items-center gap-4 mb-2">
-                  <Mail className="text-orange" />{" "}
-                  kamila.wlodarczyk@dobremiejsce-fizjoterapia.pl
-                </div>
-                <div className="flex items-center gap-4 mb-2">
-                  <PhoneCall className="text-orange" /> 539 937 132
-                </div>
-              </div> */}
               <a
                 className="inline-block whitespace-nowrap rounded-full bg-orange px-5 py-4 mb-8  text-white no-underline shadow-lg hover:bg-heavy hover:text-white"
                 href="https://dobremiejscefizjoterapiadzieci.booksy.com/"
@@ -353,6 +344,247 @@ export default function Zespol() {
                   "Ból stawu kolanowego 2025",
                   "Pourazowa niestabilność stawu skokowego 2025",
                   "Medycyna sportowa w pracy fizjoterapeuty i trenera 2025",
+                ].map((item, index) => (
+                  <li key={index} className="ml-10 pl-4">
+                    <p className="relative -top-2">{item}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          {/* </div> */}
+        </section>
+
+        <section className="mt-20">
+          {/* <div className="container mx-auto mt-20 px-4 pb-12 pt-40 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:pb-20"> */}
+          <div
+            className="mb-8 flex flex-col gap-4 lg:flex-row lg:gap-16"
+            id="michal-handzlik"
+          >
+            <img
+              src="/michal-matuszak.webp"
+              alt="Michał Matuszak"
+              className="m-0 w-40 rounded-full p-0 lg:w-auto lg:mb-auto"
+            />
+
+            <div className="text-gray">
+              <h1 className="mb-4 font-heading text-4xl md:text-5xl font-bold max-w-5xl">
+                Michał Matuszak
+                <span className="block  mt-2 mb-2 text-2xl max-w-5xl font-normal">
+                  {" "}
+                  Osteopata i fizjoterapeuta dziecięcy Katowice
+                </span>{" "}
+              </h1>
+
+              {/* <div className="mb-6 text:lg md:text-xl lg:text-2xl max-w-5xl ">
+                <div className="flex items-center gap-4 mb-2">
+                  <Mail className="text-orange" />{" "}
+                  martyna.cesarczyk@dobremiejsce-fizjoterapia.pl
+                </div>
+                <div className="flex items-center gap-4 mb-2">
+                  <PhoneCall className="text-orange" /> 577 611 464
+                </div>
+              </div> */}
+              <a
+                className="inline-block whitespace-nowrap rounded-full bg-orange px-5 py-4 mb-8  text-white no-underline shadow-lg hover:bg-heavy hover:text-white"
+                href="https://dobremiejscefizjoterapiadzieci.booksy.com/"
+                target="_blank"
+                rel="noreferrer noopener nofollow"
+              >
+                Umów wizytę
+              </a>
+
+              <p className="text-md lg:text-lg mb-6 font-bold">
+                Jestem pogodnym, uśmiechniętym osteopatą i fizjoterapeutą z
+                wieloletnim stażem którego praca jest jednocześnie pasją.
+              </p>
+
+              <p className="text-md lg:text-lg mb-6">
+                Moim celem jest leczenie i pomaganie na najwyższym poziomie, z
+                tego powodu systematycznie biorę udział w szkoleniach z zakresu
+                Osteopatii i Fizjoterapii prowadzonych przez najlepszych
+                specjalistów z tej dziedziny na świecie. Poza pracą pasjonuje
+                mnie jazda na rowerze, gotowanie oraz podróże.
+              </p>
+
+              <p className="font-semibold mb-6">Wykształcenie:</p>
+              <ul
+                className="text-md flex flex-col gap-2 font-medium lg:gap-4 lg:text-lg"
+                style={{ listStyleImage: 'url("/checkmark.png")' }}
+              >
+                {[
+                  "Absolwent Śląskiego Uniwersytetu Medycznego w Katowicach na Wydziale Nauk o Zdrowiu, kierunek – Fizjoterapia",
+                  "Absolwent międzynarodowej Szkoły Osteopatii (5-letniej): Flanders International College of Osteopathy",
+                ].map((item, index) => (
+                  <li key={index} className="ml-10 pl-4">
+                    <p className="relative -top-2">{item}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          {/* </div> */}
+        </section>
+
+        <section className="mt-20">
+          {/* <div className="container mx-auto mt-20 px-4 pb-12 pt-40 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:pb-20"> */}
+          <div
+            className="mb-8 flex flex-col gap-4 lg:flex-row lg:gap-16"
+            id="michal-handzlik"
+          >
+            <img
+              src="/michal-handzlik.webp"
+              alt="Michał Handzlik"
+              className="m-0 w-40 rounded-full p-0 lg:w-auto lg:mb-auto"
+            />
+
+            <div className="text-gray">
+              <h1 className="mb-4 font-heading text-4xl md:text-5xl font-bold max-w-5xl">
+                Michał Handzlik D.O.
+                <span className="block  mt-2 mb-2 text-2xl max-w-5xl font-normal">
+                  {" "}
+                  Dyplomowany osteopata, fizjoterapeuta
+                </span>{" "}
+              </h1>
+
+              {/* <div className="mb-6 text:lg md:text-xl lg:text-2xl max-w-5xl ">
+                <div className="flex items-center gap-4 mb-2">
+                  <Mail className="text-orange" />{" "}
+                  martyna.cesarczyk@dobremiejsce-fizjoterapia.pl
+                </div>
+                <div className="flex items-center gap-4 mb-2">
+                  <PhoneCall className="text-orange" /> 577 611 464
+                </div>
+              </div> */}
+              <a
+                className="inline-block whitespace-nowrap rounded-full bg-orange px-5 py-4 mb-8  text-white no-underline shadow-lg hover:bg-heavy hover:text-white"
+                href="https://dobremiejscefizjoterapiadzieci.booksy.com/"
+                target="_blank"
+                rel="noreferrer noopener nofollow"
+              >
+                Umów wizytę
+              </a>
+
+              <p className="text-md lg:text-lg mb-6 font-bold">
+                W swojej pracy stawiam na uważność, bezpieczeństwo i
+                indywidualne podejście do każdego pacjenta. Jako dyplomowany
+                osteopata i fizjoterapeuta pomagam osobom dorosłym, dzieciom
+                oraz niemowlętom w łagodzeniu bólu, poprawie funkcjonowania
+                ciała i odzyskaniu komfortu w codziennym życiu.
+              </p>
+
+              <p className="text-md lg:text-lg mb-6">
+                Ukończyłem fizjoterapię na Śląskim Uniwersytecie Medycznym w
+                Katowicach oraz 4-letnie studia osteopatyczne w Still Academy of
+                Osteopathy w Warszawie. Stale rozwijam swoje kompetencje
+                uczestnicząc w kursach i szkoleniach.
+              </p>
+
+              <p className="text-md lg:text-lg mb-6">
+                W terapii kieruję się holistycznym spojrzeniem na organizm,
+                szukając przyczyn dolegliwości, a nie tylko ich objawów.
+              </p>
+              <p className="font-semibold mb-6">Kwalifikacje:</p>
+              <ul
+                className="text-md flex flex-col gap-2 font-medium lg:gap-4 lg:text-lg"
+                style={{ listStyleImage: 'url("/checkmark.png")' }}
+              >
+                {[
+                  "Szkoła Osteopatii Pediatrycznej (dr George Stylian D.O., GradCertOsteoPaed) 2025/nadal",
+                  "Staw skroniowo-żuchwowy diagnostyka i terapia 2025",
+                  "Podstawowe wzorce Ontogenezy Motorycznej 2025",
+                  "Znaczenie badania odruchów oraz próby trakcji w różnych jednostkach chorobowych 2025",
+                  "Osteopatia Pediatryczna moduł I i II (Marie Caroline Wilieme D.O.) 2024",
+                  "Ocena postawy ciała u dzieci i młodzieży w kierunku wykrywania nieprawidłowości w jej obrębie 2024",
+                  "Staw kolanowy - badanie i strategia leczenia 2023",
+                  "Choroba zwyrodnieniowa stawu kolanowego 2023",
+                  "Suche igłowanie - medyczna akupunktura – moduł I 2020",
+                  "Terapia manualna wg Standardów IFOMPT 10 modułów zakończonych egzaminem MT I 2016/2019",
+                  "PNF Basic 2017",
+                  "Mobilizacji tkanek miękkich 2014/2015",
+                  "Aktywne i pasywne techniki rozluźniania mięśniowo-powięziowego 2014/2015",
+                  "Masażu głębokiego tkanek miękkich 2014/2015",
+                  "Terapii punktów spustowych 2014/2015",
+                  "Integracja strukturalna w oparciu o łańcuchy mięśniowo-powięziowe” – Kurs podstawowy 2014/2015",
+                  "Kinesiotaping - kurs podstawowy 2013",
+                ].map((item, index) => (
+                  <li key={index} className="ml-10 pl-4">
+                    <p className="relative -top-2">{item}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          {/* </div> */}
+        </section>
+
+        <section className="mt-20">
+          {/* <div className="container mx-auto mt-20 px-4 pb-12 pt-40 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:pb-20"> */}
+          <div
+            className="mb-8 flex flex-col gap-4 lg:flex-row lg:gap-16"
+            id="jakub-smistek"
+          >
+            <img
+              src="/jakub-smistek.webp"
+              alt="Jakub Śmistek"
+              className="m-0 w-40 rounded-full p-0 lg:w-auto lg:mb-auto"
+            />
+
+            <div className="text-gray">
+              <h1 className="mb-4 font-heading text-4xl md:text-5xl font-bold max-w-5xl">
+                Jakub Śmistek
+                <span className="block  mt-2 mb-2 text-2xl max-w-5xl font-normal">
+                  {" "}
+                  Fizjoterapeuta, certyfikowany terapeuta integracji
+                  sensorycznej
+                </span>{" "}
+              </h1>
+
+              {/* <div className="mb-6 text:lg md:text-xl lg:text-2xl max-w-5xl ">
+                <div className="flex items-center gap-4 mb-2">
+                  <Mail className="text-orange" />{" "}
+                  martyna.cesarczyk@dobremiejsce-fizjoterapia.pl
+                </div>
+                <div className="flex items-center gap-4 mb-2">
+                  <PhoneCall className="text-orange" /> 577 611 464
+                </div>
+              </div> */}
+              <a
+                className="inline-block whitespace-nowrap rounded-full bg-orange px-5 py-4 mb-8  text-white no-underline shadow-lg hover:bg-heavy hover:text-white"
+                href="https://dobremiejscefizjoterapiadzieci.booksy.com/"
+                target="_blank"
+                rel="noreferrer noopener nofollow"
+              >
+                Umów wizytę
+              </a>
+
+              <p className="text-md lg:text-lg mb-6 font-bold">
+                Na co dzień pracuje z dziećmi z wadami postawy, wadami stóp,
+                MPD, autyzmem, aspergerem, zespółem Downa oraz zaburzeniami
+                procesów integracji sensorycznej. W swojej pracy łączę różne
+                metody fizjoterapeutyczne aby uzyskać jak najlepsze efekty
+                terapeutyczne. Każdy plan terapii przygotowuje indywidualnie,
+                tak aby był nie tylko skuteczny, ale też ciekawy i motywujący.
+              </p>
+
+              <p className="text-md lg:text-lg mb-6">
+                Dla mnie fizjoterapia to nie tylko praca, ale sposób patrzenia
+                na świat — pełen empatii, cierpliwości i wiary w potencjał
+                każdego dziecka. Wierzę, że nawet najmniejsze postępy są
+                wielkimi zwycięstwami, a każdy dzień w gabinecie to szansa, by
+                pomóc komuś poczuć się silniejszym. Poza swoją pracą lubie
+                również podróżę górskie, bieganie, taniec, a dla relaksu lubię
+                zanurzyć się w media w świecie fantazy lub grać w gry
+                komputerowe.
+              </p>
+
+              <p className="font-semibold mb-6">Wykształcenie:</p>
+              <ul
+                className="text-md flex flex-col gap-2 font-medium lg:gap-4 lg:text-lg"
+                style={{ listStyleImage: 'url("/checkmark.png")' }}
+              >
+                {[
+                  "Absolwent Uniwersytetu Jana Kochanowskiego w Kielcach na Collegium Medicum, kierunek - Fizjoterapia",
                 ].map((item, index) => (
                   <li key={index} className="ml-10 pl-4">
                     <p className="relative -top-2">{item}</p>
